7664bdef46c7f6e2472a5261e70f8d74a04e2c30
klieng <klieng@paciolan.com>
2023-11-26 21:39:01 -0800

    remove all files currently

--- scripts/fuzzy.sh
+++
@@ -1,6 +0,0 @@
-function vx() {
-   filesToEdit=$(/usr/local/bin/fzf)
-   if (( ${#filesToEdit[@]} != 0 )); then
-       vim $filesToEdit
-   fi
-}
--- scripts/kitty.sh
+++
@@ -1,7 +0,0 @@
-function kterm() {
-    kitty +kitten ssh $1
-}
-
-function kbuild() {
-    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin installer=nightly
-}
