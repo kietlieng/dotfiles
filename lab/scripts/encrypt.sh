81730fc67a6909c8d3abec3b80ee7d3e2faee13f
klieng <klieng@paciolan.com>
2024-09-26 16:25:10 -0700

    Add encrypt function

---
+++ lab/scripts/encrypt.sh
@@ -0,0 +1,17 @@
+function encrypt() {
+  if [ "$1" ]
+  then
+    #/usr/bin/openssl des3 -in $1 -out "$1.enc"
+    #openssl des3 -in $1 -out "$1.enc"
+    openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 1000000 -salt -in $1 -out "$1.enc"
+    /bin/mv "$1.enc" $1
+  fi
+}
+
+function decrypt() {
+  if [[ "$1" ]]; then
+    #/usr/bin/openssl des3 -d -in $1
+    #openssl des3 -d -in $1
+    openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 1000000 -salt -in $1 -d 
+  fi
+}
