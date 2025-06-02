dcaadc27b536cfe2932dc8c56dd3271d48e0e3dd
klieng <klieng@paciolan.com>
2025-06-01 19:42:38 -0700

    fix indent

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,63 +5,64 @@ local set = vim.opt
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
 end
 
 set.rtp:prepend(lazypath)
 
 
 -- lazystart
-require('lazy').setup( {
+require('lazy').setup({
     { import = "plugins" },
     { import = "themes" },
   },
   {
 
-   ui = { border = 'rounded' },
-   dev = { path = vim.g.projects_dir },
-   install = {
-       -- Do not automatically install on startup.
-       missing = true,
-   },
-   -- Don't bother me when tweaking plugins.
-   change_detection = {
-     notify = false
-   },
-   -- None of my plugins use luarocks so disable this.
-   rocks = {
-       enabled = false,
-   },
-   performance = {
-     rtp = {
+    ui = { border = 'rounded' },
+    dev = { path = vim.g.projects_dir },
+    install = {
+      -- Do not automatically install on startup.
+      missing = true,
+    },
+    -- Don't bother me when tweaking plugins.
+    change_detection = {
+      notify = false
+    },
+    -- None of my plugins use luarocks so disable this.
+    rocks = {
+      enabled = false,
+    },
+    performance = {
+      rtp = {
 
-       -- Stuff I don't use.
-       disabled_plugins = {
+        -- Stuff I don't use.
+        disabled_plugins = {
 
-         -- 'theme-catppuccin',
-         -- 'theme-challenger-deep',
-         -- 'theme-cobalt2',
-         -- 'theme-darcula',
-         -- 'theme-darkvoid',
-         -- 'theme-embark',
-         -- 'theme-everforest',
-         -- 'theme-material',
-         -- 'theme-mellow',
-         -- 'theme-neofusion',
-         -- 'theme-oceanic-material',
-         -- 'theme-onedark',
-         -- 'theme-onedarkpro',
+          -- 'theme-catppuccin',
+          -- 'theme-challenger-deep',
+          -- 'theme-cobalt2',
+          -- 'theme-darcula',
+          -- 'theme-darkvoid',
+          -- 'theme-embark',
+          -- 'theme-everforest',
+          -- 'theme-material',
+          -- 'theme-mellow',
+          -- 'theme-neofusion',
+          -- 'theme-oceanic-material',
+          -- 'theme-onedark',
+          -- 'theme-onedarkpro',
 
-       },
-     },
-   },
+        },
+      },
+    },
+
   }
 )
 
