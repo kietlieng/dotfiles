7877959f06a7d48204b6af3004692dba5f8686b7
klieng <klieng@paciolan.com>
2025-09-17 12:08:23 -0700

    message feature.

--- lab/scripts/messages.sh
+++ lab/scripts/messages.sh
@@ -1,4 +1,4 @@
-function getcode() {
+function mcode() {
 
   local code=$(sqlite3 ~/Library/Messages/chat.db "select text from message order by ROWID desc" | grep -i -m 1 "google" | grep -o "[0-9]\+")
   echo -n "$code" | pbcopy
