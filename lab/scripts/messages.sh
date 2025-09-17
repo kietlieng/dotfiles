687c3cff67140a8645533714964de6f572620ea6
klieng <klieng@paciolan.com>
2025-09-17 12:04:52 -0700

    add message retrieval

--- lab/scripts/messages.sh
+++ lab/scripts/messages.sh
@@ -1,6 +1,21 @@
 function getcode() {
 
   local code=$(sqlite3 ~/Library/Messages/chat.db "select text from message order by ROWID desc" | grep -i -m 1 "google" | grep -o "[0-9]\+")
-  echo "$code" | pbcopy
+  echo -n "$code" | pbcopy
 
  }
+
+function mlast() {
+
+  modeNumber=20
+
+  if [[ $# -gt 0 ]]; then
+  
+    modeNumber="$1"
+    shift
+
+  fi
+
+  sqlite3 ~/Library/Messages/chat.db "select text from message order by ROWID desc limit $modeNumber"
+
+}
