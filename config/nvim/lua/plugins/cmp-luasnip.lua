dda909bf33e71b7e4f01b18919f2727b0e7eb302
klieng <klieng@paciolan.com>
2026-04-08 14:43:21 -0700

    moved files properly

--- config/nvim/lua/plugins/cmp-luasnip.lua
+++ config/nvim/lua/plugins/cmp-luasnip.lua
@@ -1,4 +1,10 @@
 return {
-  'saadparwaiz1/cmp_luasnip',
-  event = "VeryLazy",
+  'L3MON4D3/LuaSnip',
+  -- event = "VeryLazy",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  version = '2.*',
+  build = 'make install_jsregexp',
+  config = function()
+    require("luasnip.loaders.from_vscode").lazy_load()  -- lead friendly-snippets support into luasnip
+  end
 }
