ef4d1af3664feec630e6511fc06f5a8f45453c67
klieng <klieng@paciolan.com>
2026-04-09 10:50:14 -0700

    update

--- config/nvim/lua/plugins/cmp-main.lua
+++ config/nvim/lua/plugins/cmp-main.lua
@@ -2,6 +2,7 @@ return {
   'hrsh7th/nvim-cmp',
   dependencies = {
 
+		
 		-- -- lsp and language 
 		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/cmp-nvim-lsp-signature-help',
@@ -19,8 +20,11 @@ return {
 		-- nice options
 
 		-- 'hrsh7th/cmp-calc',      -- Math
+
+		'hrsh7th/cmp-git',       -- git
+		'nvim-lua/plenary.nvim', -- git Required
 		'f3fora/cmp-spell',      -- Spelling
-		'hrsh7th/cmp-cmdline',		-- cmdpaths
+		'hrsh7th/cmp-cmdline',   -- cmdpaths
 
   },
   config = function()
@@ -38,8 +42,8 @@ return {
       mapping = cmp.mapping.preset.insert({
         ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
-						-- cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            -- cmp.select_next_item()
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
           elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           else
@@ -82,7 +86,7 @@ return {
 						  -- or require('cmp.config.context').in_syntax_group('String')
 						end,
 						preselect_correct_word = true
-					}
+					},
 				},
 				{
 					name = 'path',
@@ -95,7 +99,6 @@ return {
 				{ name = 'cmdline' },
 
 			}),
-
 			formatting = {
 
 				format = function(entry, vim_item)
@@ -111,6 +114,7 @@ return {
 						path                    = '[Path]',
 						cmdline                 = '[Cmd]',
 						-- calc                    = '[Calc]',
+						-- calc                    = '[Calc]',
 						spell                   = '[Spell]',
 
 					})[entry.source.name] or ('[?' .. entry.source.name .. ']')
@@ -119,25 +123,60 @@ return {
 				end
 
 			},
+			github = {
+					issues = {
+							filter = "all", -- assigned, created, mentioned, subscribed, all, repos
+							limit = 100,
+							state = "open", -- open, closed, all
+					},
+					mentions = {
+							limit = 100,
+					},
+			},
+			gitlab = {
+					issues = {
+							limit = 100,
+							state = "opened", -- opened, closed, all
+					},
+					mentions = {
+							limit = 100,
+					},
+					merge_requests = {
+							limit = 100,
+							state = "opened", -- opened, closed, locked, merged
+					},
+			},
     })
 
+		cmp.setup.filetype( "gitcommit", {
+			sources = cmp.config.sources({
+				{ name = 'git' },
+			},
+			{
+				{ name = 'buffer' },
+			})
+		})
 
+		-- find I don't like it 
 		-- Combined setup for both search and command mode
-    cmp.setup.cmdline({ '/', '?' }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
-    })
+    -- cmp.setup.cmdline({ '/', '?' }, {
+    --   mapping = cmp.mapping.preset.cmdline(),
+    --   sources = {
+    --     { name = 'buffer' }
+    --   }
+    -- })
 
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
-      })
-    })
+		-- find that I don't need it
+    -- cmp.setup.cmdline(':', {
+    --   mapping = cmp.mapping.preset.cmdline(),
+    --   sources = cmp.config.sources({
+    --     { name = 'path' }
+    --   }, {
+    --     { name = 'cmdline' }
+    --   })
+    -- })
+
+		require('cmp_git').setup()
 
   end,
 }
